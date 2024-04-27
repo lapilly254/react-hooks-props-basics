437f5287175ffbaac42efb905e267cb428b6df42
Adida <adida.barack@student.moringaschool.com>
2024-04-27 19:54:28 +0300

    Solved Lab

---
+++ src/components/Links.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Links (props){
+    <div>
+        <h1>Links</h1>
+        <a href="{props.github}">{props.github}</a>
+        <a href="{props.linkedin}">{props.linkedin}</a>
+    </div>
+}
+
+export default Links
